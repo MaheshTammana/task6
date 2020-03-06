0deeed2aa09acbd6e97fa803e12c86c8a7ed0237
lenovo <lenovo@DESKTOP-EA8BM88>
2020-03-06 15:54:07 +0530

    hai

---
+++ collections/src/main/java/collections/Main.java
@@ -0,0 +1,22 @@
+package collections;
+
+public class Main
+{
+public static void main(String [] args) 
+{
+ List<Integer> list = new List();
+list.add(1);
+
+
+list.add(2);
+list.add(3);
+System.out.println(list);
+list.remove(2);
+System.out.println(list);
+System.out.println( list.get(0) );
+System.out.println( list.get(1) );
+ System.out.println(list.size());
+
+    }
+
+}
